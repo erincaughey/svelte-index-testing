b6d8ef8be181d392230242e4f87bca260462fe67
Erin Caughey <erin.renae9@gmail.com>
2019-08-06 16:59:32 -0500

    data implimentation

--- human-voter-guide/src/App.svelte
+++ human-voter-guide/src/App.svelte
@@ -1,10 +1,20 @@
 <script>
+	import { onMount } from 'svelte';
 	import Question from './Question.svelte';
+
+	let questions = [];
+
+	onMount(async () => {
+		const res = await fetch(`questions.json`);
+		questions = await res.json();
+	});
 </script>
 
 <style>
 </style>
 
 <div class="flex-container">
-<Question/>
+{#each questions as question, i}
+	<Question category = {question.category} question = {question.question} answer = {question.answer} />
+{/each}
 </div>
--- human-voter-guide/src/Question.svelte
+++ human-voter-guide/src/Question.svelte
@@ -1,21 +1,28 @@
 <script>
+    import { fly } from 'svelte/transition';
 
+    export let category;
+    export let question;
+    export let answer;
+
+    let active = false;
+
+    function handleClick() {
+		active = !active;
+	}
 </script>
 
 <style>
-/* .anchor{
-    position: relative;
-    top: -100px;
-} */
 .question-box{
     display: flex;
-    margin: 10% 0;
-    padding: 20px;
+    margin: 5px 0;
+    padding: 5px;
     border: 1px solid #009bff;
     background-color: #d4d4d4;
     box-shadow: 2px 2px 2px #ccc;
     box-sizing: border-box;
     width: 100%;
+    flex-basis: 90vw;
 }
 .qa {
     flex-basis: 90vw;
@@ -23,34 +30,47 @@
 .voter-question{
     display:flex;
     flex-flow: row wrap;
-    align-items: baseline;
+    align-items: center;
     font-family: 'Unify Sans', Tahoma, sans-serif;
+    cursor: pointer;
+}
+.voter-question:hover {
+  color: #009bff;
+}
+.voter-question:before{
+    font-size: 24px;
+    padding: 5px 10px;
+    content: '+';
+    color: #009bff;
+    font-weight: 900;
     cursor: pointer;
 }
 .voter-answer {
     display: flex;
+    padding: 20px;
     flex-flow: row wrap;
     font-family: 'Unify Serif', Georgia, serif;
-    
 }
-.expand-question {
-    font-size: 18px;
-    color: #009bff;
-    font-weight: 900;
-    align-self: center;
-    cursor: pointer;
-}
-
-.voter-question:hover {
-  color: #009bff;
+@media(max-width: 440px){
+    .voter-question{
+        position: relative;
+    }
+    .voter-question:before{
+        font-size: 20px;
+        position: absolute;
+        bottom: -50%;
+        right: 0;
+    }
 }
 </style>
 
-<div class="question-box">
-<!-- <div class="anchor" id={cat}></div> -->
+<div class="question-box" on:click={handleClick} transition:fly="{{ y: 10, duration: 1000 }}">
     <div class="qa">
-        <h4 class="voter-question">This is a question</h4>
-        <div class="voter-answer">This is your answer</div>
+        <h4 class="voter-question">{question}</h4>
+        {#if active}
+            <div class="voter-answer">
+                {answer}
+            </div>
+        {/if}
     </div>
-    <div class="expand-question">+</div>
 </div>
