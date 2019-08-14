42dbf6eda4076210244102a30540ea025be5fdbf
Erin Caughey <erin.renae9@gmail.com>
2019-08-14 17:31:23 -0500

    top 30 svelte setup

--- top-30/src/App.svelte
+++ top-30/src/App.svelte
@@ -1,11 +1,28 @@
 <script>
-	export let name;
+	import Card from './Card.svelte';
+	import MobileCard from './MobileCard.svelte';
+
+	let restaurants = [];
+
+	onMount(async () => {
+		const res = await fetch(`restaurants.json`);
+		restaurants = await res.json();
+	});
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+.flex-container{
+	display: flex;
+	flex-flow: row wrap;
+	max-width: 900px;
+	margin: 0 auto;
+	padding: 20px;
+}
 </style>
 
-<h1>Hello {name}!</h1>
+<div class="flex-container">
+{#each restaurants as restaurant, i}	
+	<Card name = {restaurant.restaurant} description = {restaurant.description} image = {restaurant.image} food = {restaurant.food-type} price = {restaurant.price-range} neighborhood = {restaurant.neighborhood}/>
+	<!-- <MobileCard name = {restaurant.restaurant} description = {restaurant.description} image = {restaurant.image} food = {restaurant.food-type} price = {restaurant.price-range} neighborhood = {restaurant.neighborhood}/> -->
+{/each}
+</div>
---
+++ top-30/src/MobileCard.svelte
@@ -0,0 +1,67 @@
+<script>
+    export let name;
+    export let description;
+    export let image;
+    export let food;
+    export let price;
+    export let neighborhood;
+    export let anchorLink;
+</script>
+
+<style>
+.rest-card-mob{
+    display: flex;
+    flex-direction: column;
+    max-width: 300px;
+    box-shadow: 2px 2px 2px #434343;
+    border: 1px solid #ccc;
+}
+.rest-img-mob{
+    width: 100%;
+}
+.rest-card-mob h4{
+    font-family: 'Unify Sans', Tahoma, sans-serif;
+    padding-left: 10px;
+}
+.rest-info{
+    flex-basis: 50vw;
+}
+.rest-text{
+    padding: 10px;
+    font-size: 15px;
+    font-family: 'Unify Sans', Tahoma, sans-serif;
+    color: rgb(145, 145, 145);
+}
+.rest-text a{
+    color: #A46583;
+}
+.rest-filters{
+    display: flex;
+    flex-direction: row;
+    padding: 10px 0;
+}
+.rest-item{
+    font-family: 'Unify Sans', Tahoma, sans-serif;
+    flex-basis: 30vw;
+    padding: 0 10px;
+}
+.label-1{
+    color: #8A32A3;
+}
+</style>
+
+<div class="rest-card-mob">
+    <div class="rest-img-mob">
+        <img src="{image}">
+    </div>
+    <h4 class="rest-name">{name}</h4>
+    <div class="rest-text">
+        {description}
+        <a href="{anchorLink}">Read more &rarr;</a>
+    </div>
+    <div class="rest-filters">
+        <div class="rest-item"><span class="label-1">{food}</span></div>
+        <div class="rest-item"><i class="fas fa-map-marker"></i> {neighborhood}</div>
+        <div class="rest-item"><i class="fas fa-dollar-sign"></i><i class="fas fa-dollar-sign"></i>{price}</div>
+    </div>
+</div>
